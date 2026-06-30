d074aa7edfdfee28c78c0d6af009e6539dd481f1
neuron3 <quellkrafts@gmail.com>
2026-06-30 11:46:12 -0600

    feat: started math lib with a simple point struct for tests purposes for now

---
+++ math/linalg/point.h
@@ -0,0 +1,14 @@
+typedef struct XPoint2D x_point_2d_t;
+
+typedef struct XPoint2D {
+  double x;
+  double y;
+} x_point_2d_t;
+
+typedef struct XPoint3D x_point_3d_t;
+
+typedef struct XPoint3D {
+  double x;
+  double y;
+  double z;
+} x_point_3d_t;
